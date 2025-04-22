43bcc344460ae86f00724f461814d6e9c223ee6d
Narasimha Mutyam <nmutyam@gmail.com>
2025-04-22 15:50:11 -0500

    Hello world Commit
    Hello world Commit

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+const version = 2 + 1
+</script>
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="hello">
+    Hello Nuxt {{ version }}!
   </div>
 </template>
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
