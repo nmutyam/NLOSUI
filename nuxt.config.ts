7cfe4ab8edfa628d7b60985faa0d1db9b35bad0c
Narasimha Mutyam <nmutyam@gmail.com>
2025-04-24 15:26:54 -0500

    New update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
+
+import Aura from '@primeuix/themes/aura';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2025-04-22"
+  compatibilityDate: "2025-04-22",
+  ssr:true
 })
