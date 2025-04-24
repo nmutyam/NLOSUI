96661f9ada42994cd7dbc96854d36cfba0a67fb0
Narasimha Mutyam <nmutyam@gmail.com>
2025-04-24 12:33:55 -0500

    Update style
    Update style

--- app.vue
+++ app.vue
@@ -7,6 +7,10 @@ const version = 2 + 1
   </div>
 </template>
 <style scoped>
-  @import "tailwindcss";
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
 
 </style>
